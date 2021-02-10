9b8ebe18f6bf0a58d92801a02e9124d22659395f
Loevenich, Mathis <m.loevenich@fz-juelich.de>
2021-02-10 14:36:58 +0100

    add test for the scraper

--- tests/test_ups_api_scraper.py
+++ tests/test_ups_api_scraper.py
@@ -1,12 +1,80 @@
 
 import pytest
+import getpass
+import os
+import sys
 from exporter.ups_api_scraper import UPSScraper
 
 
-@pytest.fixture
-def ups_scraper():
-    pass
+def vcr_config():
+    return {
+        "filter_headers": ["authorization"],
+        "ignore_hosts": ['https://localhost:4430/rest/mbdetnrs/1.0/oauth2/token']
+    }
 
 
-def test_login():
-    pass
+@pytest.mark.vcr()
+def test_ups_web_ui():
+    if not os.path.exists(
+            sys.prefix +
+            '/ups-exporter/tests/cassettes/test_ups_web_ui.yaml'
+    ):
+        scraper = UPSScraper(
+            'https://localhost:4430',
+            input('Username:'),
+            getpass.getpass('Password:'),
+            insecure=bool(input('Insecure (true|false):'))
+        )
+        measures = scraper.get_measures()
+    else:
+        scraper = UPSScraper(
+            'https://localhost:4430',
+            None,
+            None,
+            insecure=True
+        )
+        measures = scraper.get_measures()
+
+    assert measures.get('ups_id') == '1'
+
+    ups_inputs = measures.get('ups_inputs')
+    assert list(ups_inputs) == [
+        '@id', 'id', 'measures', 'status', 'phases'
+    ]
+    assert list(ups_inputs.get('measures')) == [
+        'realtime'
+    ]
+    assert list(ups_inputs.get('measures').get('realtime')) == [
+        'frequency', 'voltage', 'current'
+    ]
+
+    ups_outputs = measures.get('ups_outputs')
+    assert list(ups_outputs) == [
+        '@id', 'id', 'measures', 'status', 'phases'
+    ]
+    assert list(ups_outputs.get('measures')) == [
+        'realtime'
+    ]
+    assert list(ups_outputs.get('measures').get('realtime')) == [
+        'frequency', 'voltage', 'current', 'activePower',
+        'apparentPower', 'powerFactor', 'percentLoad'
+    ]
+
+    assert measures.get('ups_powerbank')
+    ups_powerbank = measures.get('ups_powerbank')
+    assert list(ups_powerbank) == [
+        '@id', 'specification', 'configuration',
+        'measures', 'status', 'test', 'lcm', 'entities'
+    ]
+    assert list(ups_powerbank.get('measures')) == [
+        'voltage', 'remainingChargeCapacity', 'remainingTime'
+    ]
+    assert type(
+        ups_powerbank.get('measures').get('voltage')
+    ) == int
+    assert type(
+        ups_powerbank.get('measures').get('remainingChargeCapacity')
+    ) == int
+    assert type(
+        ups_powerbank.get('measures').get('remainingTime')
+    ) == int
