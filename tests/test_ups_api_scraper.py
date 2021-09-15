074cb598ad546d5ddf1e4cd5ebfe20dcac34ca25
Loevenich, Mathis <m.loevenich@fz-juelich.de>
2021-09-15 12:15:37 +0200

    add: tests for login exceptions

--- tests/test_ups_api_scraper.py
+++ tests/test_ups_api_scraper.py
@@ -6,41 +6,47 @@ import json
 import vcr
 from . import CASSETTE_DIR, scrub_body
 from prometheus_eaton_ups_exporter.scraper import UPSScraper
-from prometheus_eaton_ups_exporter.scraper_globals import REST_API_PATH
+from prometheus_eaton_ups_exporter.scraper_globals import *
 
 
-@pytest.fixture(scope="function")
-def ups_scraper(ups_scraper_conf):
-    ups_name = list(ups_scraper_conf.keys())[0]
+def ups_scraper(address, auth, name, insecure=True):
     return UPSScraper(
-        ups_scraper_conf[ups_name]['address'],
-        (
-            ups_scraper_conf[ups_name]['user'],
-            ups_scraper_conf[ups_name]['password']
-        ),
-        ups_name,
-        insecure=True,
+        address,
+        auth,
+        name,
+        insecure=insecure,
         verbose=True
     )
 
 
-def test_login(ups_scraper):
+@pytest.fixture(scope="function")
+def scraper_fixture(ups_scraper_conf):
+    ups_name = list(ups_scraper_conf.keys())[0]
+    address = ups_scraper_conf[ups_name]['address']
+    auth = (
+        ups_scraper_conf[ups_name]['user'],
+        ups_scraper_conf[ups_name]['password']
+    )
+    return ups_scraper(address, auth, ups_name)
+
+
+def test_login(scraper_fixture):
     with vcr.use_cassette(
             os.path.join(CASSETTE_DIR, "api_login.yaml"),
             before_record_request=scrub_body()
     ):
-        token_type, access_token = ups_scraper.login()
+        token_type, access_token = scraper_fixture.login()
         assert token_type == "Bearer"
 
 
-def test_load_rest_api(ups_scraper):
+def test_load_rest_api(scraper_fixture):
     """Tests load_page function with rest api."""
     with vcr.use_cassette(
             os.path.join(CASSETTE_DIR, "api_load_rest.yaml"),
             before_record_request=scrub_body()
     ):
-        request = ups_scraper.load_page(
-            ups_scraper.ups_address + REST_API_PATH
+        request = scraper_fixture.load_page(
+            scraper_fixture.ups_address + REST_API_PATH
         )
         # Todo
         json_response = request.json()
@@ -53,12 +59,12 @@ def test_load_rest_api(ups_scraper):
         assert respones_keys == list(json_response.keys())
 
 
-def test_get_measures(ups_scraper):
+def test_get_measures(scraper_fixture):
     with vcr.use_cassette(
             os.path.join(CASSETTE_DIR, "api_measures.yaml"),
             before_record_request=scrub_body()
     ):
-        measures = ups_scraper.get_measures()
+        measures = scraper_fixture.get_measures()
         measures_keys = [
             'ups_id',
             'ups_inputs',
@@ -66,7 +72,7 @@ def test_get_measures(ups_scraper):
             'ups_powerbank'
         ]
         assert list(measures.keys()) == measures_keys
-        assert measures['ups_id'] == ups_scraper.name
+        assert measures['ups_id'] == scraper_fixture.name
 
         ups_inputs = measures.get('ups_inputs')
         assert list(ups_inputs) == [
@@ -99,3 +105,94 @@ def test_get_measures(ups_scraper):
         assert list(ups_powerbank.get('measures')) == [
             'voltage', 'remainingChargeCapacity', 'remainingTime'
         ]
+
+
+def conf_details(conf):
+    ups_name = list(conf.keys())[0]
+    address = conf[ups_name]['address']
+    auth = (
+        conf[ups_name]['user'],
+        conf[ups_name]['password']
+    )
+    return address, auth, ups_name
+
+
+def test_missing_schema_exception():
+    scraper = ups_scraper("", ("", ""), "")
+    with pytest.raises(LoginFailedException) as pytest_wrapped_e:
+        scraper.login()
+    assert isinstance(pytest_wrapped_e.type, LoginFailedException)
+    assert pytest_wrapped_e.value.error_code == MISSING_SCHEMA_ERROR
+
+
+def test_invalid_url_exception():
+    scraper = ups_scraper("https:0.0.0.0", ("", ""), "")
+    with pytest.raises(LoginFailedException) as pytest_wrapped_e:
+        scraper.login()
+    assert isinstance(pytest_wrapped_e.type, LoginFailedException)
+    assert pytest_wrapped_e.value.error_code == INVALID_URL_ERROR
+
+
+def test_connection_refused_exception():
+    scraper = ups_scraper("https://127.0.0.1", ("", ""), "")
+    with pytest.raises(LoginFailedException) as pytest_wrapped_e:
+        scraper.login()
+    assert isinstance(pytest_wrapped_e.type, LoginFailedException)
+    assert pytest_wrapped_e.value.error_code == CONNECTION_ERROR
+
+
+def test_certificate_exception(ups_scraper_conf):
+    address, auth, ups_name = conf_details(ups_scraper_conf)
+    scraper = ups_scraper(
+        address,
+        auth,
+        ups_name,
+        insecure=False
+    )
+    with pytest.raises(LoginFailedException) as pytest_wrapped_e:
+        with vcr.use_cassette(
+                os.path.join(CASSETTE_DIR, "certificate_exception.yaml"),
+                before_record_request=scrub_body(),
+                record_mode="new_episodes"
+        ):
+            scraper.load_page(address)
+    assert isinstance(pytest_wrapped_e.type, LoginFailedException)
+    assert pytest_wrapped_e.value.error_code == CERTIFICATE_VERIFY_FAILED
+
+
+def test_login_timeout_exception(ups_scraper_conf):
+    address, _, ups_name = conf_details(ups_scraper_conf)
+    scraper = ups_scraper(
+        address,
+        ("a", "b"),
+        ups_name
+    )
+    with pytest.raises(LoginFailedException) as pytest_wrapped_e:
+        with vcr.use_cassette(
+                os.path.join(CASSETTE_DIR, "login_timeout_exception.yaml"),
+                record_mode="new_episodes"
+        ):
+            scraper.login()
+    assert isinstance(pytest_wrapped_e.type, LoginFailedException)
+    assert pytest_wrapped_e.value.error_code == TIMEOUT_ERROR
+
+
+@vcr.use_cassette(
+    os.path.join(CASSETTE_DIR, "auth_failed_exception.yaml"),
+    before_record_request=scrub_body(),
+    record_mode="new_episodes"
+)
+def test_auth_failed_exception(ups_scraper_conf):
+    address, _, ups_name = conf_details(ups_scraper_conf)
+    scraper = ups_scraper(
+        address,
+        (ups_scraper_conf[ups_name]['user'], "abc"),
+        ups_name
+    )
+    with pytest.raises(LoginFailedException) as pytest_wrapped_e:
+        scraper.load_page(address + REST_API_PATH)
+    assert isinstance(pytest_wrapped_e.type, LoginFailedException)
+    assert pytest_wrapped_e.value.error_code == AUTHENTICATION_FAILED
+
+
+
