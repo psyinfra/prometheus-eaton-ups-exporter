e94523bd9bdcb4f513f9ed06ac8a7b0a831fc7b8
Loevenich, Mathis <m.loevenich@fz-juelich.de>
2021-09-15 14:24:21 +0200

    use vcrpy as decorator

--- tests/test_ups_api_scraper.py
+++ tests/test_ups_api_scraper.py
@@ -30,81 +30,81 @@ def scraper_fixture(ups_scraper_conf):
     return ups_scraper(address, auth, ups_name)
 
 
+@vcr.use_cassette(
+    os.path.join(CASSETTE_DIR, "api_login.yaml"),
+    before_record_request=scrub_body()
+)
 def test_login(scraper_fixture):
-    with vcr.use_cassette(
-            os.path.join(CASSETTE_DIR, "api_login.yaml"),
-            before_record_request=scrub_body()
-    ):
-        token_type, access_token = scraper_fixture.login()
-        assert token_type == "Bearer"
+    token_type, access_token = scraper_fixture.login()
+    assert token_type == "Bearer"
 
 
+@vcr.use_cassette(
+    os.path.join(CASSETTE_DIR, "api_load_rest.yaml"),
+    before_record_request=scrub_body()
+)
 def test_load_rest_api(scraper_fixture):
     """Tests load_page function with rest api."""
-    with vcr.use_cassette(
-            os.path.join(CASSETTE_DIR, "api_load_rest.yaml"),
-            before_record_request=scrub_body()
-    ):
-        request = scraper_fixture.load_page(
-            scraper_fixture.ups_address + REST_API_PATH
-        )
-        # Todo
-        json_response = request.json()
-        respones_keys = [
-            '@id', 'id', 'identification', 'specification',
-            'configuration', 'ups', 'status', 'inputs',
-            'avr', 'outputs', 'inverters', 'chargers',
-            'backupSystem', 'bypass', 'rectifiers', 'outlets'
-        ]
-        assert respones_keys == list(json_response.keys())
+    request = scraper_fixture.load_page(
+        scraper_fixture.ups_address + REST_API_PATH
+    )
+    # Todo
+    json_response = request.json()
+    respones_keys = [
+        '@id', 'id', 'identification', 'specification',
+        'configuration', 'ups', 'status', 'inputs',
+        'avr', 'outputs', 'inverters', 'chargers',
+        'backupSystem', 'bypass', 'rectifiers', 'outlets'
+    ]
+    assert respones_keys == list(json_response.keys())
 
 
+@vcr.use_cassette(
+    os.path.join(CASSETTE_DIR, "api_measures.yaml"),
+    before_record_request=scrub_body()
+)
 def test_get_measures(scraper_fixture):
-    with vcr.use_cassette(
-            os.path.join(CASSETTE_DIR, "api_measures.yaml"),
-            before_record_request=scrub_body()
-    ):
-        measures = scraper_fixture.get_measures()
-        measures_keys = [
-            'ups_id',
-            'ups_inputs',
-            'ups_outputs',
-            'ups_powerbank'
-        ]
-        assert list(measures.keys()) == measures_keys
-        assert measures['ups_id'] == scraper_fixture.name
+    measures = scraper_fixture.get_measures()
+    measures_keys = [
+        'ups_id',
+        'ups_inputs',
+        'ups_outputs',
+        'ups_powerbank'
+    ]
+    assert list(measures.keys()) == measures_keys
+    assert measures['ups_id'] == scraper_fixture.name
 
-        ups_inputs = measures.get('ups_inputs')
-        assert list(ups_inputs) == [
-            '@id', 'id', 'measures', 'status', 'phases'
-        ]
-        assert list(ups_inputs.get('measures')) == [
-            'realtime'
-        ]
-        assert list(ups_inputs.get('measures').get('realtime')) == [
-            'frequency', 'voltage', 'current'
-        ]
+    ups_inputs = measures.get('ups_inputs')
+    assert list(ups_inputs) == [
+        '@id', 'id', 'measures', 'status', 'phases'
+    ]
+    assert list(ups_inputs.get('measures')) == [
+        'realtime'
+    ]
+    assert list(ups_inputs.get('measures').get('realtime')) == [
+        'frequency', 'voltage', 'current'
+    ]
 
-        ups_outputs = measures.get('ups_outputs')
-        assert list(ups_outputs) == [
-            '@id', 'id', 'measures', 'status', 'phases'
-        ]
-        assert list(ups_outputs.get('measures')) == [
-            'realtime'
-        ]
-        assert list(ups_outputs.get('measures').get('realtime')) == [
-            'frequency', 'voltage', 'current', 'activePower',
-            'apparentPower', 'powerFactor', 'percentLoad'
-        ]
+    ups_outputs = measures.get('ups_outputs')
+    assert list(ups_outputs) == [
+        '@id', 'id', 'measures', 'status', 'phases'
+    ]
+    assert list(ups_outputs.get('measures')) == [
+        'realtime'
+    ]
+    assert list(ups_outputs.get('measures').get('realtime')) == [
+        'frequency', 'voltage', 'current', 'activePower',
+        'apparentPower', 'powerFactor', 'percentLoad'
+    ]
 
-        ups_powerbank = measures.get('ups_powerbank')
-        assert list(ups_powerbank) == [
-            '@id', 'specification', 'configuration',
-            'measures', 'status', 'test', 'lcm', 'entities'
-        ]
-        assert list(ups_powerbank.get('measures')) == [
-            'voltage', 'remainingChargeCapacity', 'remainingTime'
-        ]
+    ups_powerbank = measures.get('ups_powerbank')
+    assert list(ups_powerbank) == [
+        '@id', 'specification', 'configuration',
+        'measures', 'status', 'test', 'lcm', 'entities'
+    ]
+    assert list(ups_powerbank.get('measures')) == [
+        'voltage', 'remainingChargeCapacity', 'remainingTime'
+    ]
 
 
 def conf_details(conf):
@@ -121,7 +121,7 @@ def test_missing_schema_exception():
     scraper = ups_scraper("", ("", ""), "")
     with pytest.raises(LoginFailedException) as pytest_wrapped_e:
         scraper.login()
-    assert isinstance(pytest_wrapped_e.type, LoginFailedException)
+    assert pytest_wrapped_e.type == LoginFailedException
     assert pytest_wrapped_e.value.error_code == MISSING_SCHEMA_ERROR
 
 
@@ -129,7 +129,7 @@ def test_invalid_url_exception():
     scraper = ups_scraper("https:0.0.0.0", ("", ""), "")
     with pytest.raises(LoginFailedException) as pytest_wrapped_e:
         scraper.login()
-    assert isinstance(pytest_wrapped_e.type, LoginFailedException)
+    assert pytest_wrapped_e.type == LoginFailedException
     assert pytest_wrapped_e.value.error_code == INVALID_URL_ERROR
 
 
@@ -137,7 +137,7 @@ def test_connection_refused_exception():
     scraper = ups_scraper("https://127.0.0.1", ("", ""), "")
     with pytest.raises(LoginFailedException) as pytest_wrapped_e:
         scraper.login()
-    assert isinstance(pytest_wrapped_e.type, LoginFailedException)
+    assert pytest_wrapped_e.type == LoginFailedException
     assert pytest_wrapped_e.value.error_code == CONNECTION_ERROR
 
 
